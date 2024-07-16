9806df5ea3ec9852ddf9ef9129327a9dc297e824
kdeepthi123 <44495139+kdeepthi123@users.noreply.github.com>
2024-07-16 14:14:36 -0400

    added a comment

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import CssBaseline from '@mui/material/CssBaseline';
-
+//hello
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
