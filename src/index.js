61cf6258cd1eb33a1b39ac9a56bc16c2e543535b
kdeepthi123 <44495139+kdeepthi123@users.noreply.github.com>
2024-07-16 14:14:46 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import CssBaseline from '@mui/material/CssBaseline';
+<<<<<<< Updated upstream
 //hello
+=======
+//added a  change
+>>>>>>> Stashed changes
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
